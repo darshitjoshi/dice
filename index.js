de462eb44cfaf8b7255e1fd8ad5cddefa861816b
Darshit Joshi <92263919+darshitjoshi@users.noreply.github.com>
2022-11-30 22:56:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var r1 = Math.random();
 r1 = r1 * 6 + 1;
 r1 = Math.floor(r1);
 
-var rI1 = "images/dice" + r1 + ".png";
+var rI1 = "dice" + r1 + ".png";
 document.querySelector(".di1").setAttribute("src", rI1);
 
 var r2 = Math.random();
 r2 = r2 * 6 + 1;
 r2 = Math.floor(r2);
 
-var rI2 = "images/dice" + r2 + ".png";
+var rI2 = "dice" + r2 + ".png";
 document.querySelector(".di2").setAttribute("src", rI2);
 
 if (r1 > r2) {
